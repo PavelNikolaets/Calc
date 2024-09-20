203235a0574ef01bc336c7a8d288c8acc537bca9
PavelNikolaets <63319992+PavelNikolaets@users.noreply.github.com>
2024-09-20 23:51:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,70 +1,91 @@
 ﻿
-using System.Diagnostics;
-
-char[] operations = [];
-int[] nums = [];
-
-string temporary = "";
-int index = 0;
-
-string input = Console.ReadLine();
-
-for (int i = 0; i <= input.Length; i++)
+static bool IsOperator(char symbol)
 {
-    if (i != input.Length)
-    {
-        if (char.IsDigit(input[i]))
-        {
-            temporary += input[i];
-        }
-        else
-        {
-            Array.Resize(ref nums, nums.Length+1);
-            Array.Resize(ref operations, operations.Length+1);
-            nums[index] = Convert.ToInt32(temporary);
-            temporary = "";
-            operations[index] = input[i];
-            index++;
-        }
-    }
-    else
-    {
-        Array.Resize(ref nums, nums.Length+1);
-        nums[index] = Convert.ToInt32(temporary);
-    }
+    List<char> operators = ['-','+','*','/'];
+    return operators.Contains(symbol) ? true : false;
 }
 
-Console.WriteLine ($"Числа: {string.Join(",",nums)} \nОператоры: {string.Join(",",operations)}");
-
-int result = 0;
-foreach (char item in operations)
+static string multiplication(string num1, string num2)
 {
-    switch(item)
-    {
-        case '+':
-            result = nums[0]+nums[1];
-            nums[0] = result;
-            break;
-        
-        case '-':
-            result = nums[0]-nums[1];
-            nums[0] = result;
-            break;
-        
-        default:
-            Console.Error.WriteLine("Неизвестный оператор!");
-            Process.GetCurrentProcess().Kill();
-            break;
-    }
-    if (nums.Length > 2)
-    {
-        Array.Copy(nums,2,nums,1,nums.Length-2);
-        Array.Resize(ref nums, nums.Length-1);
-    }
-    else
-    {
-        Array.Resize(ref nums, nums.Length-1);
-    }
+    return Convert.ToString(Convert.ToDouble(num1) * Convert.ToDouble(num2));
 }
 
-Console.WriteLine($"{string.Join(',',nums)}");
+static string division(string num1, string num2)
+{
+    return Convert.ToString(Convert.ToDouble(num1) / Convert.ToDouble(num2));
+}
+
+static string addition(string num1, string num2)
+{
+    return Convert.ToString(Convert.ToDouble(num1) + Convert.ToDouble(num2));
+}
+
+static string subtraction(string num1, string num2)
+{
+    return Convert.ToString(Convert.ToDouble(num1) - Convert.ToDouble(num2));
+}
+
+{
+    while (true)
+    {
+        List<string> line = new();
+        string temporary = "";
+
+        string input = Console.ReadLine();
+
+        if (input == "exit")
+        {
+            break;
+        }
+
+        for (int i = 0; i <= input.Length; i++)
+        {
+            if (i != input.Length)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    temporary += input[i];
+                }
+                else if (IsOperator(input[i]))
+                {
+                    line.Add(Convert.ToString(temporary));
+                    line.Add(Convert.ToString(input[i]));
+                    temporary = "";
+                }
+            }
+            else 
+            {
+                line.Add(temporary);
+            }
+        }
+        Console.WriteLine(string.Join(",", line));
+        while (line.Count != 1)
+        {    
+            if (line.Contains("*"))
+            {
+                int index = line.FindIndex(x => x == "*");
+                line[index-1] = multiplication(line[index-1], line[index+1]);
+                line.RemoveRange(index, 2);
+            }
+            else if (line.Contains("/"))
+            {
+                int index = line.FindIndex(x => x == "/");
+                line[index-1] = division(line[index-1], line[index+1]);
+                line.RemoveRange(index, 2);
+            }
+            else if (line.Contains("+"))
+            {
+                int index = line.FindIndex(x => x == "+");
+                line[index-1] = addition(line[index-1], line[index+1]);
+                line.RemoveRange(index, 2);
+            }
+            else if (line.Contains("-"))
+            {
+                int index = line.FindIndex(x => x == "-");
+                line[index-1] = subtraction(line[index-1], line[index+1]);
+                line.RemoveRange(index, 2);
+            }   
+        }
+        Console.WriteLine($"Результат: {string.Join(", ", line)}");
+    }
+}
